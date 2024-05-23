1fb510f7a0718b6df53bbbfc3d5833922e0d2a04
Luca Lambiase <luca.lambia@gmail.com>
2024-05-23 14:08:33 +0200

    reset components + app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data() {
+    return {
+      autore: "Luca Lambiase"
+    }
+  },
+  methods: {
+    welcome() {
+      console.log("Benvenuto nell'applicazione di La Molisana, sviluppata da ", this.autore);
+    }
+  },
+  mounted() {
+    this.welcome();
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <h1>La Molisana by {{ autore }}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum quisquam, consequatur debitis ipsam fuga molestiae
+      deserunt expedita odio a labore et minima? Maxime voluptatum sint sed aperiam, voluptate animi fuga!</p>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
